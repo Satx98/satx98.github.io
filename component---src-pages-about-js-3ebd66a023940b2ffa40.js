606c0435c30dc2348df5e9a44817ab18e3f61937
Satadru Roy <satadru.roy98@gmail.com>
2021-01-17 23:16:12 +0530

    Updates

---
+++ component---src-pages-about-js-3ebd66a023940b2ffa40.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"3XHS":function(n,e,t){"use strict";t.r(e);var u=t("q1tI"),a=t.n(u);e.default=function(){return a.a.createElement("div",null)}}}]);
+//# sourceMappingURL=component---src-pages-about-js-3ebd66a023940b2ffa40.js.map
